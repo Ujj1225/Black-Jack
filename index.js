2d264a3aa14179d1214ebe2082c3a3704dc08677
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-11 06:10:15 +0545

    JS before overcoming hardcode values

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 10
+let cards= [
+    firstCard,
+    secondCard
+]
 let sum = firstCard + secondCard
 let isAlive = true
 let hasBlackjack = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+// Now lets be more specific about selection of classes and id's 
+// This is achieved using querySelector
+// In query selector we must be specific about what we are selecting so a dot for class and an # for id is mandatory here 
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector(".cards-el")
 
-if (sum < 21){
-    console.log("Do you want to draw a new card? 😊")
+function startGame(){
+    renderGame()
 }
-else if(sum == 21){
-    console.log("Wohoo! You'ev got Blackjack! 🥳")
-    hasBlackjack = true
+function renderGame(){
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " 
+    for(let i = 0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    if (sum < 21){
+        message = "Do you want to draw a new card?"
+    }
+    else if(sum == 21){
+        message = "You'ev got Blackjack!"
+        hasBlackjack = true
+    }
+    else{
+        message = "You are out of the game!"
+        isAlive = false
+    }
+    
+    messageEl.textContent = message
 }
-else{
-    console.log("You are out of the game! 😭")
-    isAlive = false
+function newcards(){
+    let card = 4;
+    sum += card;
+    cards.push(card)
+    console.log(cards)
+    renderGame();
+    console.log("Drawing new cards!")
 }
+    
